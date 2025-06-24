c9fec2f91cf7dcd8d6230bf12a56dae00f664ade
CedDesu <cedriqueorbista@gmail.com>
2025-06-24 22:51:54 +0800

    Changed animation speed of when death of npc is triggered in npc.py and main.py

--- npc.py
+++ npc.py
@@ -20,31 +20,52 @@ class NPC(AnimatedSprite):
         self.accuracy = 0.15
         self.alive = True
         self.pain = False
+        self.ray_cast_value = False
+        self.frame_counter = 0
 
     def update(self):
         self.check_animation_time()
         self.get_sprite()
         self.run_logic()
+        #self.draw_ray_cast()
 
     def animate_pain(self):
         self.animate(self.pain_images)
         if self.animation_trigger:
             self.pain = False
 
+    def animate_death(self):
+        if not self.alive:
+            if self.game.global_trigger and self.frame_counter < len(self.death_images) - 1:
+                self.death_images.rotate(-1)
+                self.image = self.death_images[0]
+                self.frame_counter += 1
+
     def run_logic(self):
         if self.alive:
+            self.ray_cast_value = self.ray_cast_player_npc()
             self.check_hit_in_npc()
+
             if self.pain:
                 self.animate_pain()
             else:
                 self.animate(self.idle_images)
+        else:
+            self.animate_death()
 
     def check_hit_in_npc(self):
-        if self.game.player.shot:
+        if self.ray_cast_value and self.game.player.shot:
             if HALF_WIDTH - self.sprite_half_width < self.screen_x < HALF_WIDTH + self.sprite_half_width:
                 self.game.player.shot = False
                 self.game.sound.npc_pain.play()
                 self.pain = True
+                self.health -= self.game.weapon.damage
+                self.check_health()
+
+    def check_health(self):
+        if self.health < 1:
+           self.alive = False
+           self.game.sound.npc_death.play()
 
     @property
     def map_pos(self):
