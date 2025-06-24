4fdb996077311fdaf0f1d34f8e1efd2df3444ece
CedDesu <cedriqueorbista@gmail.com>
2025-06-24 23:29:08 +0800

    Added possible fixes. Game is still crashing on npc pathfinding.

--- pathfinding.py
+++ pathfinding.py
@@ -35,3 +35,9 @@ class PathFinding:
 
     def get_next_nodes(self, x, y):
         return [(x + dx, y + dy) for dx, dy in self.ways if (x + dx, y + dy) not in self.game.map.world_map]
+
+    def get_graph(self):
+        for y, row in enumerate(self.map):
+            for x, col in enumerate(row):
+                if not col:
+                    self.graph[(x, y)] = self.graph.get((x, y), []) + self.get_next_nodes(x, y)
