3a3e7cff187a6ea755fa60bdd4170ee1ebb70fb5
CedDesu <cedriqueorbista@gmail.com>
2025-06-24 20:30:09 +0800

    Added non-working code for adding npc sprites using npc.py and object_handler.py

--- object_handler.py
+++ object_handler.py
@@ -1,12 +1,17 @@
 from sprite_object import *
+from npc import *
 
 class ObjectHandler:
     def __init__(self, game):
         self.game = game
         self.sprite_list = []
+        self.npc_list = []
+        self.npc_sprite_path = 'resources/sprites/npc/'
         self.static_sprite_path = 'resources/sprites/static_sprites/'
         self.anim_sprite_path = 'resources/sprites/animated_sprites/'
         add_sprite = self.add_sprite
+        add_npc = self.add_npc
+        self.npc_positions = {}
 
         # sprites
         add_sprite(AnimatedSprite(game))
@@ -25,6 +30,9 @@ class ObjectHandler:
 
     def update(self):
         [sprite.update() for sprite in self.sprite_list]
+
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
 
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
