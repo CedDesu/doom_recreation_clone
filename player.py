f86df5d9c6508e477f50657f4d1ae10297a06f19
CedDesu <cedriqueorbista@gmail.com>
2025-06-24 23:44:27 +0800

    Added game over, health, victory, and other similar messages. Game still continues to crash, will check pathfinding.

--- player.py
+++ player.py
@@ -9,6 +9,9 @@ class Player:
         self.angle = PLAYER_ANGLE
         self.shot = False
         self.health = PLAYER_MAX_HEALTH
+        self.rel = 0
+        self.health_recovery_delay = 700
+        self.time_prev = pg.time.get_ticks()
         # diagonal movement correction
         self.diag_move_corr = 1 / math.sqrt(2)
 
@@ -109,6 +112,7 @@ class Player:
     def update(self):
         self.movement()
         self.mouse_control()
+        self.recover_health()
 
     @property
     def pos(self):
